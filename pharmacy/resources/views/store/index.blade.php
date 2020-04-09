e99a88d5b8bb64875276e731e680dfac800cce6e
AhmedSalahAhmed <wad.alshaikh98@gmail.com>
2020-04-09 21:29:03 +0300

    m

--- pharmacy/resources/views/store/index.blade.php
+++ pharmacy/resources/views/store/index.blade.php
@@ -15,7 +15,7 @@
         <thead>
             <tr>
                 <th scope="col">رقم الدواء</th>
-                <th scope="col">اسم الدواء</th>
+                <th scope="col">إسم الدواء</th>
                 <th scope="col"> نوع الدواء</th>
                 <th scope="col">الشركة</th>
                 <th scope="col">الكمية</th>
